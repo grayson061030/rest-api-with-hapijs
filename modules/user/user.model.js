d02c09af3ab09da9abe4f1201fdc4930d561e291
GavinKim <dreamworld612@gmail.com>
2018-07-23 23:37:53 +0900

    사용자정보 모델에서 필드 제외처리하는 부분 삭제. 로그인할 경우 필요함. 관계모델로 가져올때 ignore fileds 처리 해야함.

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -5,17 +5,17 @@ const UserSchema = new Schema({
     username: {type: String,requied: true},
     thumbnail:{type: String, default:'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y'},
     email: {type: String, unique:true,lowercase:true},
-    password: {type:String,trim:true, select: false}, // password 필드를 출력 하려면 findOne({_id: id}).select('+password').exec......
+    password: {type:String,trim:true},
     facebook:{
-        id: {type: String,select: false},
-        accessToken: {type: String,select: false},
-        refreshToken: {type: String,select: false}
+        id: {type: String},
+        accessToken: {type: String},
+        refreshToken: {type: String}
     },
-    updated:{type:Date,select: false},
-    created:{type:Date,default:Date.now,select: false},
-    deleted:{type:Date,select: false},
-    role: {type:Number,default: 0,select: false},
-    activate:{type:Boolean,default:false,select: false} // true: deleted
+    updated:{type:Date},
+    created:{type:Date,default:Date.now},
+    deleted:{type:Date},
+    role: {type:Number,default: 0},
+    activate:{type:Boolean,default:false} // true: deleted
 });
 
 module.exports = mongoose.model('User',UserSchema);
